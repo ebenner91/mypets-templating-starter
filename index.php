61efc5c564bf78d16d5470458d80489de9bd3d4b
Elizabeth Benner <ebenner@mail.greenriver.edu>
2017-05-16 22:03:39 -0500

    made a bunch of changes

--- index.php
+++ index.php
@@ -10,7 +10,22 @@
     //Default route
     $f3->route('GET /', function($f3) {
         
-        echo "Hello!";
+        $f3->set('username', 'jshmo');
+        $f3->set('password', sha1('Password01'));
+        $f3->set('title', 'Working with Templates');
+        $f3->set('temp', 68);
+        $f3->set('color', 'purple');
+        $f3->set('radius', 10);
+        $f3->set('bookmarks', array('http://www.cnet.com', 'http://www.reddit.com/r/news', 'http://edition.cnn.com/sport'));
+        $f3->set('addresses', array('primary' =>
+                                    '1000 Apple Ln. Seattle, WA 98999',
+                                    'secondary' =>
+                                    '2510 100th Court, Sea-Tac, WA 90000'));
+        $f3->set('desserts', array('chocolate' => 'Chocolate Mousse',
+                                   'vanilla' => 'Vanilla Custard',
+                                   'strawberry' => 'Strawberry Shortcake'));
+        
+        echo Template::instance()->render('pages/info.html');
         
     });
 
---
+++ tmp/qqtwrz3ssr5m.2hbmt1l6jeqsc.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+    </head>
+    <body>
+      <h1><?= $title ?></h1>
+      <p>User logged in as <?= $username ?> using <?= $password ?></p>
+      <h3>Temperature</h3>
+      <p>Fahrenheit: <?= $temp ?> degrees</p>
+      <p>Celsius: <?= $temp - 32 * (5.0/9.0) ?> degrees</p>
+      <p>I like <?= $color ?></p>
+      <p>Circumfrence: <?= 2 * 3.14 * $radius ?></p>
+      
+      <h3>Bookmarks</h3>
+      <p>My first bookmark is: <a href="<?= $bookmarks[0] ?>"><?= $bookmarks[0] ?></a></p>
+      <ul>
+        <?php foreach (($bookmarks?:[]) as $bookmark): ?>
+            <li><a href="<?= $bookmark ?>"> <?= str_replace('http://', '', $bookmark) ; ?></a></li>
+        <?php endforeach; ?>
+      </ul>
+      
+      <h3>Addresses</h3>
+      <?php foreach (($addresses?:[]) as $key=>$value): ?>
+        <p><?= $key ?> - <?= $value ?></p>
+      <?php endforeach; ?>
+      
+      <h3>Desserts</h3>
+      <?php foreach (($desserts?:[]) as $key=>$value): ?>
+        <input type="checkbox"><?= $value ?> <br>
+      <?php endforeach; ?>
+
+      
+ 
+        
+    </body>
+</html>
